5b6504b7938e8c75daac8fa1c5c9415f10f908ae
Fabio Stoppini <stoppinifabio@gmail.com>
2024-07-08 12:35:35 +0200

    sistemato configurazione prodotti, aggiunto il carrello

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,14 +1,25 @@
 <script setup>
+import { ref } from 'vue';
 import { getProducts } from '../app.js'
 const products = await getProducts();
+let sommaPrezzi = ref(0);
+
+function handleClick(prezzo){
+
+  sommaPrezzi.value += prezzo;
+ }
+ 
 </script>
 
 <template>
+<div class="carrello"> Carrello: {{sommaPrezzi.toFixed(2)}} € </div>
+<button class="clic" @click="">Vai al carrello</button>
 <h2>Prodotti:</h2>
 <ul v-for="prod in products" class="prodotti">
-    <div class="desc">{{prod.id}} {{prod.title}} {{prod.price}}</div>
+    <div>{{prod.id}} {{prod.title}} </div>
+    <div class="desc"> Prezzo: {{prod.price}} € </div>
     <img class="immagini" :src="prod.image" alt="Product Image" />
-
+    <button class="clic" @click="handleClick(prod.price)">Acquista</button>
 </ul>
 </template>
 <style>
@@ -16,16 +27,42 @@ const products = await getProducts();
     display: flex;
     flex-direction: column;
     align-items: right;
-    max-width: 200px; 
-    max-height: 200px;
+    margin-top: 50px;
+    font-weight: 500;
+    font-size: 1.5em;
   }
-  .immagini
-  {
-    display: block; 
-  margin-top: 10px; 
+  .immagini{
+    max-width: 200px; 
+    max-height: 300px;
+    display: block;
+    float: left; 
   }
   .desc{
-    margin-bottom: 10px;
+    font-weight: 500; 
+    margin-top: 20px;
+    font-size: 1.2em;
   }
+  
+  .carrello{
+
+        font-weight: bold; /* Rende il testo in grassetto */
+        margin-top: 20px;
+        font-size: 1.5em;
+  }
+  .clic{
+    display: block;
+    width: 200px;
+    padding: 20px;
+    background-color: #f0f0f0;
+    border: 1px solid #ccc;
+    text-align: center;
+    text-decoration: none;
+    color: black;
+    margin-top: 20px;
+    cursor: pointer;
+  }
+  .clic:hover{
+            background-color: #e0e0e0;
+        }
   
 </style>
